8f0f2a7457c965f981d5b3c5df9009ed32d6d76b
lavimelis <141926557+lavimelis@users.noreply.github.com>
2024-05-19 20:11:19 +0300

    Add files via upload

---
+++ odev.dart
@@ -0,0 +1,28 @@
+void main() {
+  String isim = 'Melis';
+  print(isim);
+  String username = 'Alsan';
+  print(username);
+  int kilo = 65;
+  print(kilo);
+  double boy = 1.57;
+  print(boy);
+  bool resitDegil = true;
+  bool resit = false;
+  print(resitDegil);
+  print(resit);
+
+
+  var melisAlsan = "adım melisAlsan";
+  print(melisAlsan);
+  var soyad_isim = "soyadım Alsan";
+  print(soyad_isim);
+
+
+  String ad = "Melis Alsan";
+  int yas = 22;
+  bool resitlikDurumu = false;
+
+  print("Ben $ad, yaşım $yas, benim reşitlik durumum: $resitlikDurumu");
+}
+
